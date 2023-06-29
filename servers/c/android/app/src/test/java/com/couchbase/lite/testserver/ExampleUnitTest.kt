24352e767fa640f44e151a6192fbe8921c421d11
Pasin Suriyentrakorn <pasin@couchbase.com>
2023-06-28 19:28:07 -0700

    Add android project (#10)

---
+++ servers/c/android/app/src/test/java/com/couchbase/lite/testserver/ExampleUnitTest.kt
@@ -0,0 +1,12 @@
+package com.couchbase.lite.testserver
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest { }
