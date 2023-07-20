7e3a2846f110d92d725eac674054dafcf191097b
Jim Borden <jim.borden@couchbase.com>
2023-07-20 10:49:02 +0900

    Update config file to specify admin creds for both server and sgw
    :warn: This invalidates any existing configuration files, config.example.json has been updated and the schema refreshed

--- client/src/cbltest/configparser.py
+++ client/src/cbltest/configparser.py
@@ -2,14 +2,16 @@ from pathlib import Path
 from json import load, dumps
 from typing import Final, List, cast
 
-from .jsonhelper import _assert_contains_string_list, _get_int_or_default, _get_string_list, _assert_string_entry
+from .jsonhelper import _assert_contains_string_list, _get_int_or_default, _get_string_list, _assert_string_entry, _get_str_or_default
 
 class SyncGatewayInfo:
     """The parsed Sync Gateway information from the config file"""
 
     __hostname_key: Final[str] = "hostname"
     __port_key: Final[str] = "port"
-    __admin_port_key: Final[str] = "admin_port"
+    __admin_port_key: Final[str] = "admin_port",
+    __rbac_user_key: Final[str] = "rbac_user"
+    __rbac_password_key: Final[str] = "rbac_password"
 
     @property
     def hostname(self) -> str:
@@ -26,11 +28,50 @@ class SyncGatewayInfo:
         """Gets the port to use for admin connection"""
         return self.__admin_port
     
+    @property
+    def rbac_user(self) -> str:
+        """Gets the user to use when connecting to a CBS cluster"""
+        return self.__rbac_user
+    
+    @property
+    def rbac_password(self) -> str:
+        """Gets the password to use when connecting to a CBS cluster"""
+        return self.__rbac_password
+    
     def __init__(self, data: dict):
         self.__hostname: str = _assert_string_entry(data, self.__hostname_key)
         self.__port: int = _get_int_or_default(data, self.__port_key, 4984)
         self.__admin_port: int = _get_int_or_default(data, self.__admin_port_key, 4985)
+        self.__rbac_user: str = _get_str_or_default(data, self.__rbac_user_key, "admin")
+        self.__rbac_password: str = _get_str_or_default(data, self.__rbac_password_key, "password")
+
+class CouchbaseServerInfo:
+    """The parsed Couchbase Server info from the config file"""
+
+    __hostname_key: Final[str] = "hostname"
+    __admin_user_key: Final[str] = "admin_user"
+    __admin_password_key: Final[str] = "admin_password"
+
+    @property
+    def hostname(self) -> str:
+        """Gets the hostname of the Couchbase Server cluster instance"""
+        return self.__hostname
+        
+    @property
+    def admin_user(self) -> str:
+        """Gets the user to use when administrating a CBS cluster"""
+        return self.__admin_user
     
+    @property
+    def admin_password(self) -> str:
+        """Gets the password to use when administrating a CBS cluster"""
+        return self.__admin_password
+
+    def __init__(self, data: dict):
+        self.__hostname: str = _assert_string_entry(data, self.__hostname_key)
+        self.__admin_user: str = _get_str_or_default(data, self.__admin_user_key, "Administrator")
+        self.__admin_password: str = _get_str_or_default(data, self.__admin_password_key, "password")
+
 class ParsedConfig:
     """The parsed result of the JSON config file provided to the SDK"""
 
@@ -52,7 +93,7 @@ class ParsedConfig:
         return self.__sync_gateways
     
     @property
-    def couchbase_servers(self) -> List[str]:
+    def couchbase_servers(self) -> List[dict]:
         """The list of couchbase servers that can be interacted with"""
         return self.__couchbase_servers
     
@@ -74,7 +115,7 @@ class ParsedConfig:
             raise ValueError(f"Missing key in configuration '{self.__sgw_key}'")
         
         self.__sync_gateways = json[self.__sgw_key]
-        self.__couchbase_servers = _assert_contains_string_list(json, self.__cbs_key)
+        self.__couchbase_servers = json[self.__cbs_key]
         self.__sync_gateway_certs = _get_string_list(json, self.__sgw_certs_key)
         self.__greenboard = cast(str, json.get(self.__greenboard_key))
         self.__api_version = _get_int_or_default(json, self.__api_version_key, 1)
