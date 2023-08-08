ba630427404d3acfd1980285c7db4ada6a8478af
Pasin Suriyentrakorn <pasin@couchbase.com>
2023-08-07 19:10:56 -0700

    C Test Server : Fix warnings from windows build (#27)

--- servers/c/src/TestServer.cpp
+++ servers/c/src/TestServer.cpp
@@ -5,7 +5,6 @@
 #include "Android.h"
 #endif
 
-#include "Error.h"
 #include "Files.h"
 #include "UUID.h"
 
@@ -14,7 +13,6 @@
 #include <string>
 
 using namespace std;
-using namespace ts::support::error;
 using namespace ts::support::files;
 using namespace ts::support::key;
 
