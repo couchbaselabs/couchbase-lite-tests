6ef85ee8b25c99a65a74c2790fa564ee8106be61
Pasin Suriyentrakorn <pasin@couchbase.com>
2023-08-08 13:23:01 -0700

    C Test Server : Remove unused functions from Fleece.h

--- servers/c/src/cbl/Fleece.h
+++ servers/c/src/cbl/Fleece.h
@@ -10,13 +10,10 @@
 namespace ts_support::fleece {
     nlohmann::json toJSON(FLValue value);
 
-    void updateProperty(FLMutableDict dict, FLSlice keyPath, const nlohmann::json &value);
+    void updateProperties(FLMutableDict dict,
+                          const std::vector<std::unordered_map<std::string, nlohmann::json>> &updates);
 
-    void removeProperty(FLMutableDict dict, FLSlice keyPath);
-
-    void updateProperties(FLMutableDict dict, std::vector<std::unordered_map<std::string, nlohmann::json>> updates);
-
-    void removeProperties(FLMutableDict dict, std::vector<std::string> keyPaths);
+    void removeProperties(FLMutableDict dict, const std::vector<std::string> &keyPaths);
 
     FLDict compareDicts(FLDict dict1, FLDict dict2);
 }
