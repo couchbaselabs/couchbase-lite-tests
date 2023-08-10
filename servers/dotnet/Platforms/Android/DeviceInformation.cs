2be455222b943ffbda3a210dcfcfbf21662fa09c
Jim Borden <jim.borden@couchbase.com>
2023-08-10 11:57:12 +0900

    Add Android device information implementation to C# server

---
+++ servers/dotnet/Platforms/Android/DeviceInformation.cs
@@ -0,0 +1,18 @@
+﻿using TestServer.Services;
+using Droid = global::Android;
+
+namespace TestServer.Platforms.Android
+{
+    internal sealed class DeviceInformation : IDeviceInformation
+    {
+        private readonly IDeviceInfo _mauiInfo = DeviceInfo.Current;
+
+        public string Model => $"{_mauiInfo.Manufacturer} / {_mauiInfo.Model}";
+
+        public string SystemName => "Android";
+
+        public string SystemVersion => Droid.OS.Build.VERSION.Release ?? Droid.OS.Build.VERSION.Codename ?? "unknown";
+
+        public string SystemApiVersion => $"{(int)Droid.OS.Build.VERSION.SdkInt}";
+    }
+}
