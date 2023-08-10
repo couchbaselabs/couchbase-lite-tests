33eb9e5d73f334de3bbc0f5abf45e431685180e4
Jim Borden <jim.borden@couchbase.com>
2023-08-10 10:39:11 +0900

    Add a few more smoke tests

---
+++ client/smoke_tests/test_start_replicator.py
@@ -0,0 +1,28 @@
+from cbltest import CBLPyTest
+from cbltest.api.error import CblTestServerBadResponseError
+from cbltest.api.error_types import ErrorDomain
+from cbltest.api.replicator import Replicator
+from cbltest.api.replicator_types import ReplicatorActivityLevel
+from cbltest.api.database import Database
+import pytest
+
+class TestStartReplicator:
+    @pytest.mark.asyncio
+    async def test_invalid_database(self, cblpytest: CBLPyTest) -> None:
+        repl = Replicator(Database(cblpytest.request_factory, 0, "fake"),
+                          "ws://localhost:4984/db")
+        with pytest.raises(CblTestServerBadResponseError, match="returned 400"):
+            await repl.start()
+
+    @pytest.mark.asyncio
+    async def test_bad_endpoint(self, cblpytest: CBLPyTest) -> None:
+        dbs = await cblpytest.test_servers[0].create_and_reset_db("names", ["db1"])
+        db = dbs[0]
+
+        repl = Replicator(db, "ws://foo:4984/db")
+        await repl.start()
+        status = await repl.wait_for(ReplicatorActivityLevel.STOPPED)
+        assert status.error is not None and ErrorDomain.equal(status.error.domain, ErrorDomain.CBL) \
+            and status.error.code == 5002
+        
+        
