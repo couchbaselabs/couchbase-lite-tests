4a4086c6d67da53b1a4dcc109c6a18a84b916c9d
Pasin Suriyentrakorn <pasin@couchbase.com>
2023-08-15 13:56:45 -0700

    C Server : Move android to platforms folder

--- servers/c/platforms/android/app/src/main/java/com/couchbase/lite/testserver/MainActivity.kt
+++ servers/c/platforms/android/app/src/main/java/com/couchbase/lite/testserver/MainActivity.kt
@@ -3,13 +3,17 @@ package com.couchbase.lite.testserver
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.couchbase.lite.testserver.ui.theme.TestServerTheme
 import com.couchbase.lite.testserver.util.AssetUtil
 import java.io.File
@@ -35,7 +39,7 @@ class MainActivity : ComponentActivity() {
             TestServerTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("CBL-C Test Server")
+                    Title("Test Server for C")
                 }
             }
         }
@@ -48,17 +52,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "$name!",
+fun Title(name: String, modifier: Modifier = Modifier) {
+    Box(
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "$name",
+            fontSize = 30.sp,
+            textAlign = TextAlign.Center,
             modifier = modifier
-    )
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TitlePreview() {
     TestServerTheme {
-        Greeting("Android")
+        Title("Android")
     }
 }
