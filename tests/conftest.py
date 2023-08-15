30f9bc397cb5eeefef9466b5a1e7b9c060d07a79
Jim Borden <jim.borden@couchbase.com>
2023-08-15 11:23:26 +0900

    Some tweaks to python client
    1. Throw an error if the wrong type is passed to upsert_document since python doesn't enforce types and this one is easy to get wrong
    2. Add a get_document method to the sync gateway API to retrieve a single document
    3. Fix the test name in the http_log requests (and, as chance would have it, any error responses)
    4. Eliminate warnings about various classes not being created "in an async context" by making the fixture creating CBLPyTest an async fixture
    5. Override event_loop to be for the entire session rather than be created per function.  Otherwise, the couchbase server connection will be ruined each time when the event loop is closed.  Now the same object will live through the entire session and keep its UUID
    6. Filter out a python warning that is coming from inside the Couchbase SDK so that we can have runs with 0 warnings

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,27 @@
 import os
 from pathlib import Path
 from cbltest import CBLPyTest
+import asyncio
 
 import pytest
+import pytest_asyncio
 
-@pytest.fixture
-def cblpytest(request: pytest.FixtureRequest) -> CBLPyTest:
+@pytest.fixture(scope="session")
+def event_loop():
+    loop = asyncio.get_event_loop()
+    yield loop
+    loop.close()
+
+# Async to avoid DeprecationWarnings about aiohttp client session
+@pytest_asyncio.fixture(scope="session")
+async def cblpytest(request: pytest.FixtureRequest) -> CBLPyTest:
     config = request.config.getoption("--config")
     log_level = request.config.getoption("--cbl-log-level")
     test_props = request.config.getoption("--test-props")
     output = request.config.getoption("--output")
     return CBLPyTest(config, log_level, test_props, output)
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def dataset_path() -> Path:
     script_path = os.path.abspath(os.path.dirname(__file__))
     return Path(script_path, "..", "dataset")
