e8c1cceadc0707a5c28463fd542178a6840843ac
Pasin Suriyentrakorn <pasin@couchbase.com>
2023-08-16 14:00:54 -0700

    C Server : Copy iOS built apps to ouput binary directory

--- servers/c/scripts/build_ios.sh
+++ servers/c/scripts/build_ios.sh
@@ -15,7 +15,13 @@ BLD_NUM=${3}
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 ASSETS_DIR="${SCRIPT_DIR}/../assets"
+
+BUILD_DIR="${SCRIPT_DIR}/../build"
+BUILD_BIN_DIR="${BUILD_DIR}/out/bin"
+DOWNLOAD_DIR="${BUILD_DIR}/download"
+
 IOS_DIR="${SCRIPT_DIR}/../platforms/ios"
+FRAMEWORKS_DIR="${IOS_DIR}/Frameworks"
 BUILD_SIMULATOR_DIR="${IOS_DIR}/build_simulator"
 BUILD_DEVICE_DIR="${IOS_DIR}/build_device"
 
@@ -25,13 +31,10 @@ cp -f ../../../dataset/*.cblite2.zip dataset
 cp -f ../../../environment/sg/cert/cert.* cert
 popd
 
-# Go to iOS project directory:
-pushd "${IOS_DIR}" > /dev/null
-
 # Download and Unzip CBL:
-pushd Frameworks > /dev/null
-rm -rf CouchbaseLite.xcframework
-rm -f *.txt
+rm -rf "${DOWNLOAD_DIR}" 2> /dev/null
+mkdir -p "${DOWNLOAD_DIR}"
+pushd "${DOWNLOAD_DIR}" > /dev/null
 
 if [ -z "$BLD_NUM" ]
 then
@@ -43,17 +46,26 @@ else
     curl -O http://latestbuilds.service.couchbase.com/builds/latestbuilds/couchbase-lite-c/${VERSION}/${BLD_NUM}/${ZIP_FILENAME}
 fi
 unzip ${ZIP_FILENAME}
-rm ${ZIP_FILENAME}
-rm *.txt
+cp -r CouchbaseLite.xcframework "${FRAMEWORKS_DIR}"
 popd > /dev/null
+
+# Go to iOS project directory:
+pushd "${IOS_DIR}" > /dev/null
 
 # Get vendor
 pushd vendor > /dev/null
 sh cmake.sh
 popd > /dev/null
 
+# Build
 set -o pipefail # Get xcpretty to report failures
 xcodebuild -scheme TestServer -sdk iphonesimulator -configuration Release -derivedDataPath "${BUILD_SIMULATOR_DIR}" | xcpretty
 xcodebuild -scheme TestServer -sdk iphoneos -configuration Release -derivedDataPath "${BUILD_DEVICE_DIR}" -allowProvisioningUpdates | xcpretty
+
+# Copy artifacts
+rm -rf "${BUILD_BIN_DIR}"
+mkdir -p "${BUILD_BIN_DIR}"
+cp -r "$BUILD_SIMULATOR_DIR/Build/Products/Release-iphonesimulator/TestServer.app" "${BUILD_BIN_DIR}/TestServer-Simulator.app"
+cp -r "$BUILD_DEVICE_DIR/Build/Products/Release-iphoneos/TestServer.app" "${BUILD_BIN_DIR}"
 
 popd > /dev/null
