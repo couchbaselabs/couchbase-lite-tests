7c76c38b5da1387f71f49efbcb81ab67151d597b
Jim Borden <jim.borden@couchbase.com>
2023-09-09 11:24:02 +0900

    Modify bundle name and Android properties
    This allows xharness to more easily launch both iOS and Android

--- servers/dotnet/Platforms/Android/MainActivity.cs
+++ servers/dotnet/Platforms/Android/MainActivity.cs
@@ -4,7 +4,7 @@ using Android.OS;
 
 namespace TestServer;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(Theme = "@style/Maui.SplashTheme",  Name = "com.couchbase.dotnet.testserver.MainActivity", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
 }
