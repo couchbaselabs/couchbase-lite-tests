e6ab9727fb09d27b3d368b6160564ec75406e8b5
Jim Borden <jim.borden@couchbase.com>
2023-09-21 11:55:32 +0900

    Fix an issue with Powershell Core and run_winui.ps1
    The Get-AppxPackage family of calls must be run using Windows powershell (i.e. Powershell 5)

--- servers/dotnet/scripts/run_winui.ps1
+++ servers/dotnet/scripts/run_winui.ps1
@@ -1,5 +1,5 @@
 Write-Host "Removing existing package..."
-Get-AppxPackage bf1b9964-631c-4489-91fa-a04e7f3f3765* | Remove-AppxPackage
+powershell -Command "Get-AppxPackage bf1b9964-631c-4489-91fa-a04e7f3f3765* | Remove-AppxPackage"
 
 $InstallScript = Get-ChildItem -Recurse -Path .\bin\Release\ -Include Install.ps1 | Select-Object -First 1 -ExpandProperty FullName
 if(-not ($InstallScript)) {
