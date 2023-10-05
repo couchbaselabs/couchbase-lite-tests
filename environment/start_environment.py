fc64f5763c329ff616c13695d186a49b7f1a6495
G. Blake Meike <blake.meike@couchbase.com>
2023-10-05 16:50:56 -0700

    start_environment.py handles docker compose 2.21 and later

--- environment/start_environment.py
+++ environment/start_environment.py
@@ -1,39 +1,45 @@
 #!/usr/bin/env python3
 
+import sys, time
 import subprocess
-import time
 from json import loads
 
 def start_environment():
     print("Bringing up docker compose...")
     subprocess.run(["docker", "compose", "up", "-d"])
     print()
-    ps_output = loads(subprocess.check_output(["docker", "compose", "ps", "--format", "json"]).decode())
-    sg_count = 0
-    for entry in ps_output:
-        if "cbl-test-sg" in entry["Name"]:
-            sg_count += 1
-            print(f"Found SG #{sg_count}: {entry['Name']}")
 
-    retry = True
+    sg_count = 0
+    output = subprocess.check_output(["docker", "compose", "ps", "--format", "json"]).decode().splitlines()
+    for line in output:
+        json = loads(line)
+        if "cbl-test-sg" in json["Name"]:
+            sg_count += 1
+            print(f"Found SG #{sg_count}: {json['Name']}")
+
+    found = True
     seen = set()
-    while retry:
-        ready_sg = 0
-        print("\tWaiting for environment...")
+    while sg_count > 0:
+        if found:
+            print(f"\tWaiting for {sg_count} SGs..", end="")
+            found = False
+        else:
+            sys.stdout.write(".")
+            sys.stdout.flush()
+            time.sleep(2)
+
         output = subprocess.check_output(["docker", "compose", "logs"]).decode().splitlines()
         for line in output:
             if "Sync Gateway is up" in line:
-                ready_sg += 1
-                retry = ready_sg != sg_count
                 which = line.split()[0]
                 if not which in seen:
                     seen.add(which)
-                    print(f"\t{which} ready!")
-                break
+                    sg_count -= 1
+                    print("")   
+                    print(f"\tSG {which} is ready!")
+                    found = True
 
-        if retry:
-            time.sleep(2)
-        
+    print("")
     print("Done!")
 
 if __name__ == "__main__":
