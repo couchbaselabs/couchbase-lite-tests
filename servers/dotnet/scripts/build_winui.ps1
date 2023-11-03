77961c14979016f4ad2abd58275c3920061b765c
Pasin Suriyentrakorn <pasin@couchbase.com>
2023-11-02 18:34:14 -0700

    Dotnet : Update build script to copy dataset and blobs

--- servers/dotnet/scripts/build_winui.ps1
+++ servers/dotnet/scripts/build_winui.ps1
@@ -1,1 +1,9 @@
+
+# Copy dataset
+Push-Location $PSScriptRoot\..\Resources\Raw
+Copy-Item $PSScriptRoot\..\..\..\dataset\server\dbs\*.zip .
+Copy-Item $PSScriptRoot\..\..\..\dataset\server\blobs . -Recurse
+Pop-Location
+
+# Build
 dotnet publish .\testserver.csproj -c Release -f net7.0-windows10.0.19041.0
