0eed686e72533a0a470e5726f5dcc5d30d43afcb
G. Blake Meike <blake.meike@couchbase.com>
2023-11-06 13:14:17 -0800

    One working auto-purge test and one not so much

--- client/src/cbltest/assertions.py
+++ client/src/cbltest/assertions.py
@@ -1,7 +1,13 @@
-from typing import Any
+from typing import List, Any
 
 def _assert_not_null(arg: Any, name: str) -> Any:
     if arg is None:
-        raise TypeError(f"{name} cannot be null!")
+        raise TypeError(f"{name} must not be null!")
     
+    return arg
+
+def _assert_not_empty(arg: List[Any], name: str) -> Any:
+    if len(arg) <= 0:
+        raise TypeError(f"{name} must not be empty!")
+
     return arg
