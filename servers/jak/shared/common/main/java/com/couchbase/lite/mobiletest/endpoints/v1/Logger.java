dfa05577bbe30273d8c11ac8f8161ffe3050349f
G. Blake Meike <blake.meike@couchbase.com>
2023-11-21 13:43:46 -0800

    Add logging endpoint

---
+++ servers/jak/shared/common/main/java/com/couchbase/lite/mobiletest/endpoints/v1/Logger.java
@@ -0,0 +1,50 @@
+//
+// Copyright (c) 2023 Couchbase, Inc All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package com.couchbase.lite.mobiletest.endpoints.v1;
+
+import androidx.annotation.NonNull;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import com.couchbase.lite.mobiletest.trees.TypedMap;
+import com.couchbase.lite.mobiletest.util.Log;
+
+
+public class Logger {
+    private static final String KEY_MESSAGE = "message";
+
+    private static final Set<String> LEGAL_LOG_KEYS;
+    static {
+        final Set<String> l = new HashSet<>();
+        l.add(KEY_MESSAGE);
+        LEGAL_LOG_KEYS = Collections.unmodifiableSet(l);
+    }
+    /* log a message from the client */
+
+
+    @NonNull
+    public Map<String, Object> log(@NonNull TypedMap req) {
+        req.validate(LEGAL_LOG_KEYS);
+
+        final String msg = req.getString(KEY_MESSAGE);
+        Log.i("CLIENT", (msg == null) ? "" : msg);
+
+        return Collections.emptyMap();
+    }
+}
