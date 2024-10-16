3d71d5ffdec2842770fab059846bb8d3fabbbdb3
Pasin Suriyentrakorn <pasin@couchbase.com>
2024-10-15 22:09:15 -0700

    Fix C Test Server CBLHeader.h

--- servers/c/src/cbl/CBLHeader.h
+++ servers/c/src/cbl/CBLHeader.h
@@ -6,14 +6,16 @@
 #ifdef __APPLE__
 
 #include <TargetConditionals.h>
-
-#ifdef TARGET_OS_OSX
+// Note : Not using TARGET_OS_OSX here because it doesn't work with CLion editor when the
+//        other headers include this header for some reason but works fine during build.
+#if !TARGET_OS_IPHONE && !TARGET_OS_SIMULATOR
 #define CBL_HEADER(X) STRINGIFY(cbl/X)
 #define FLEECE_HEADER(X) STRINGIFY(fleece/X)
 #else
 #define CBL_HEADER(X) STRINGIFY(CouchbaseLite/X)
 #define FLEECE_HEADER(X) STRINGIFY(CouchbaseLite/X)
 #endif
+
 #else
 #define CBL_HEADER(X) STRINGIFY(cbl/X)
 #define FLEECE_HEADER(X) STRINGIFY(fleece/X)
