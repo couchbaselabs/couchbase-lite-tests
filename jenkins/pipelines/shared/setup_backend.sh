4ee36364d5f53204a93b092b01848e45ad2c9e0f
Jim Borden <jim.borden@couchbase.com>
2024-10-24 12:50:00 +0900

    Port setup backend script to unix
    Fix Windows lack of argument

---
+++ jenkins/pipelines/shared/setup_backend.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -e
+
+SCRIPT_ROOT=$(dirname "$0")
+
+if test -t 1; then
+    ncolors=$(tput colors)
+    if test -n "$ncolors" && test $ncolors -ge 8; then
+        BOLD="$(tput bold)"
+        UNDERLING="$(tput smul)"
+        STANDOUT="$(tput smso)"
+        NORMAL="$(tput sgr0)"
+        black="$(tput setaf 0)"
+        RED="$(tput setaf 1)"
+        GREEN="$(tput setaf 2)"
+        YELLOW="$(tput setaf 3)"
+        BLUE="$(tput setaf 4)"
+        MAGENTA="$(tput setaf 5)"
+        CYAN="$(tput setaf 6)"
+        WHITE="$(tput setaf 7)"
+    fi
+fi
+
+function write-banner() {
+    echo
+    echo ${GREEN}===== $1 =====${NORMAL}
+    echo
+}
+
+sgw_url=""
+if [ $# -eq 1 ]; then
+    sgw_url=$1
+fi
+
+write-banner "Stopping existing environment"
+pushd $SCRIPT_ROOT/../../../environment
+docker compose down # Just in case it didn't get shut down cleanly
+
+write-banner "Building Couchbase Server Image"
+docker compose build cbl-test-cbs
+
+write-banner "Building logslurp Image"
+docker compose build cbl-test-logslurp
+
+if [ "$sgw_url" != "" ]; then
+    write-banner "Building Sync Gateway Image"
+    docker compose build cbl-test-sg --build-arg SG_DEB="$sgw_url"
+fi
+
+write-banner "Starting Backend"
+./start_environment.py
