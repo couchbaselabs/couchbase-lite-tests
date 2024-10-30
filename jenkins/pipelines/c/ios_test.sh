5ce62567bc65e310be5b8c23642d06cae8c6f531
Pasin Suriyentrakorn <pasin@couchbase.com>
2024-10-29 20:34:58 -0700

    Use setup_backend script in C and iOS pipeline (#77)
    * Updated the test scripts to derive directories from the script directory.
    * Used setup_bacakend script in C and iOS pipeline.

--- jenkins/pipelines/c/ios_test.sh
+++ jenkins/pipelines/c/ios_test.sh
@@ -3,9 +3,14 @@
 EDITION=${1}
 VERSION=${2}
 BLD_NUM=${3}
+SGW_URL=${4}
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+TEST_SERVER_DIR="${SCRIPT_DIR}/../../../servers/c"
+TESTS_DIR="${SCRIPT_DIR}/../../../tests"
 
 echo "Build Test Server"
-pushd servers/c > /dev/null
+pushd "${TEST_SERVER_DIR}" > /dev/null
 ./scripts/build_ios.sh device ${EDITION} ${VERSION} ${BLD_NUM}
 
 echo "Run Test Server"
@@ -13,14 +18,13 @@ pushd build/out/bin > /dev/null
 ios kill com.couchbase.CBLTestServer || true
 ios install --path=TestServer.app
 ios launch com.couchbase.CBLTestServer
-
 popd > /dev/null
 
 popd > /dev/null
 
 echo "Start environment"
-pushd environment > /dev/null
-./start_environment.py
+pushd "${SCRIPT_DIR}/../shared" > /dev/null
+./setup_backend.sh ${SGW_URL}
 popd > /dev/null
 
 echo "Run tests"
@@ -30,7 +34,7 @@ python3.10 -m venv venv
 pip install -r requirements.txt
 
 rm -f "config.c-ios.json"
-cp "${WORKSPACE}/jenkins/pipelines/c/config.c-ios.json" .
+cp "${SCRIPT_DIR}/config.c-ios.json" .
 pytest -v --no-header -W ignore::DeprecationWarning --config config.c-ios.json
 deactivate
 popd
--- jenkins/pipelines/c/linux_test.sh
+++ jenkins/pipelines/c/linux_test.sh
@@ -3,9 +3,14 @@
 EDITION=${1}
 VERSION=${2}
 BLD_NUM=${3}
+SGW_URL=${4}
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+TEST_SERVER_DIR="${SCRIPT_DIR}/../../../servers/c"
+TESTS_DIR="${SCRIPT_DIR}/../../../tests"
 
 echo "Build Test Server"
-pushd servers/c > /dev/null
+pushd "${TEST_SERVER_DIR}" > /dev/null
 ./scripts/build_linux.sh ${EDITION} ${VERSION} ${BLD_NUM}
 
 echo "Run Test Server"
@@ -17,17 +22,18 @@ popd > /dev/null
 popd > /dev/null
 
 echo "Start environment"
-pushd environment > /dev/null
-./start_environment.py
+pushd "${SCRIPT_DIR}/../shared" > /dev/null
+./setup_backend.sh ${SGW_URL}
 popd > /dev/null
 
 echo "Run tests"
-pushd tests > /dev/null
+pushd "${TESTS_DIR}" > /dev/null
 python3.10 -m venv venv
 . venv/bin/activate
 pip install -r requirements.txt
+
 rm -f "config.c.json"
-cp "${WORKSPACE}/jenkins/pipelines/c/config.c.json" .
+cp "${SCRIPT_DIR}/config.c.json" .
 pytest -v --no-header -W ignore::DeprecationWarning --config config.c.json
 deactivate
 popd > /dev/null
