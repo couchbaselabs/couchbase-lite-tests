bd5e5e1e0e4d81233d95a0cf65e072a7ee7a7839
Jim Borden <jim.borden@couchbase.com>
2024-11-07 07:45:17 +0900

    Move env vars out out scripts and into Jenkinsfile for .NET (#84)
    This makes them easier to centralize.  Also change shell to sh and put the Jenkins job in charge of unlocking the keychain since it's only relevant over SSH.

--- jenkins/pipelines/dotnet/build_mac.sh
+++ jenkins/pipelines/dotnet/build_mac.sh
@@ -5,6 +5,5 @@ source $SCRIPT_DIR/prepare_env.sh
 
 banner "Executing build for .NET $DOTNET_VERSION Mac Catalyst"
 
-export DEVELOPER_DIR="/Applications/Xcode-16.0.0/"
 pushd $SCRIPT_DIR/../../../servers/dotnet/testserver
 $HOME/.dotnet/dotnet build -f net$DOTNET_VERSION-maccatalyst -c Release -v n
