adbe09a150a2f9d1ed243e0e087f92309f59517d
Tor Colvin <tor.colvin@couchbase.com>
2024-12-18 22:07:38 -0500

    allow mypy to work

--- .github/workflows/verify_python.sh
+++ .github/workflows/verify_python.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -e
 
 python3 -m venv venv
-source venv/bin/activate
+source ./venv/bin/activate
 pip install mypy
 pip install pytest
 pip install types-requests
 pip install types-Deprecated
 pip install ./client
 echo "Checking tests files..."
-python -m mypy tests --exclude=venv --ignore-missing-imports
+python -m mypy tests --exclude=venv
 echo "Checking client files (including smoke tests)..."
-python -m mypy client --exclude=venv --ignore-missing-imports
+python -m mypy client --exclude=venv
 deactivate
 rm -rf venv
