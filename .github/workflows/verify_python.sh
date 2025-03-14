af36eff6f02b509b314d43a6090925dea01ae5c7
Jim Borden <jim.borden@couchbase.com>
2025-03-14 12:37:56 +0900

    Roll back some overly aggressive GH action changes

--- .github/workflows/verify_python.sh
+++ .github/workflows/verify_python.sh
@@ -7,9 +7,9 @@ pip install pytest pytest-asyncio
 pip install types-requests types-Deprecated types-tqdm types-paramiko types-netifaces types-psutil
 pip install ./client
 echo "Checking tests files..."
-python -m mypy tests --exclude=venv --explicit-package-bases --check-untyped-defs
+python -m mypy tests --exclude=venv
 echo "Checking client files (including smoke tests)..."
-python -m mypy client --exclude=venv --explicit-package-bases --check-untyped-defs
+python -m mypy client --exclude=venv
 echo "Checking environment setup..."
 python -m mypy environment --exclude=venv --explicit-package-bases --check-untyped-defs
 deactivate
