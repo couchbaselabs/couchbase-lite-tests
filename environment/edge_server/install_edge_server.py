bfbd8bdc7b82f1c1b6a10dd7fafaef5c03ab1c76
barkha06 <barkha.goyal@couchbase.com>
2025-04-07 23:01:34 +0530

    Added certs and user config to installation script

--- environment/edge_server/install_edge_server.py
+++ environment/edge_server/install_edge_server.py
@@ -70,7 +70,6 @@ def install_edge_server(edge_server_ip, edge_server_config, version, build, crea
     """Installs and configures Edge Server on the given IP."""
 
     fix_hostname(edge_server_ip)
-
     package_url = f"https://latestbuilds.service.couchbase.com/builds/latestbuilds/couchbase-edge-server/{version}/{build}/couchbase-edge-server_{version}-{build}_amd64.deb"
     package_file = "/tmp/couchbase-edge-server.deb"
     config_file_path = "/opt/couchbase-edge-server/etc/config.json"
@@ -90,13 +89,52 @@ def install_edge_server(edge_server_ip, edge_server_config, version, build, crea
         run_remote_command(edge_server_ip, "sudo mkdir -p /opt/couchbase-edge-server/cert")
         run_remote_command(edge_server_ip, "sudo chmod 755 -R /opt/couchbase-edge-server/cert")
         run_remote_command(edge_server_ip, "sudo chown -R couchbase:couchbase /opt/couchbase-edge-server/cert")
-    
-    if database_path:
+
+    if create_cert:
+        print(f"Creating client certificate on {edge_server_ip}...")
+        command = f"/opt/couchbase-edge-server/bin/couchbase-edge-server --create-cert CN={edge_server_ip} /opt/couchbase-edge-server/cert/certfile_tls /opt/couchbase-edge-server/cert/keyfile_tls"
+        run_remote_command(edge_server_ip, command)
+        subprocess.run(
+            ["sshpass", "-p", "couchbase", "scp", f"root@{edge_server_ip}:/opt/couchbase-edge-server/cert/certfile_tls", "./config"],
+            check=True)
+
+    if mtls:
+        mtls_key_path="/opt/couchbase-edge-server/cert/rootkey"
+        mtls_cert_path="/opt/couchbase-edge-server/cert/rootcert"
+        print(f"Creating root certificate on {edge_server_ip}...")
+        command = f"openssl genrsa -out {mtls_key_path} 2048"
+        run_remote_command(edge_server_ip, command)
+        command=f"openssl req -x509 -new -nodes -key {mtls_key_path} -sha256 -days 365 -out {mtls_cert_path} -subj '/CN=Root'"
+        run_remote_command(edge_server_ip, command)
+        command="openssl genrsa -out /opt/couchbase-edge-server/cert/keyfile 2048"
+        run_remote_command(edge_server_ip, command)
+        command=f"openssl req -new -key  /opt/couchbase-edge-server/cert/keyfile -out  /opt/couchbase-edge-server/cert/server_csr -subj '/CN={edge_server_ip}'"
+        run_remote_command(edge_server_ip, command)
+        command=f"openssl x509 -req -in /opt/couchbase-edge-server/cert/server_csr -CA {mtls_cert_path} -CAkey {mtls_key_path} -CAcreateserial -out /opt/couchbase-edge-server/cert/certfile -days 365"
+        run_remote_command(edge_server_ip, command)
+
+    if database_path is not None:
+        print("Setting up database directory...")
         run_remote_command(edge_server_ip, "sudo mkdir -p /opt/couchbase-edge-server/database")
         run_remote_command(edge_server_ip, "sudo chmod 755 -R /opt/couchbase-edge-server/database")
-        run_remote_command(edge_server_ip, "sudo chown -R couchbase:couchbase /opt/couchbase-edge-server/database")
-        copy_file_to_remote(edge_server_ip, database_path, data_file_path)
+        run_remote_command(edge_server_ip, f"sudo chown -R couchbase:couchbase /opt/couchbase-edge-server/database")
+        print(f"Copying database file to {data_file_path}...")
+        cmd_check_sshpass = "command -v sshpass || sudo apt-get update && sudo apt-get install -y sshpass"
+        run_remote_command(edge_server_ip, cmd_check_sshpass)
+        subprocess.run(
+            ["sshpass", "-p", "couchbase", "scp", database_path, f"root@{edge_server_ip}:{data_file_path}"],
+            check=True)
+        cmd_check_unzip = "command -v unzip || sudo apt-get update && sudo apt-get install -y unzip"
+        run_remote_command(edge_server_ip,cmd_check_unzip)
         run_remote_command(edge_server_ip, f"unzip -o {data_file_path} -d /opt/couchbase-edge-server/database")
+
+    if user:
+        print(f"Creating users.json  on {edge_server_ip}...")
+        run_remote_command(edge_server_ip, "sudo mkdir -p /opt/couchbase-edge-server/users")
+        run_remote_command(edge_server_ip, "sudo chmod 755 -R /opt/couchbase-edge-server/users")
+        run_remote_command(edge_server_ip, "sudo chown -R couchbase:couchbase /opt/couchbase-edge-server/users")
+        command = f"/opt/couchbase-edge-server/bin/couchbase-edge-server --add-user /opt/couchbase-edge-server/users/users.json  admin_user --create --role admin --password password"
+        run_remote_command(edge_server_ip, command)
     
     run_remote_command(edge_server_ip, f"sudo chmod 644 {config_file_path}")
     run_remote_command(edge_server_ip, f"sudo chown -R couchbase:couchbase {config_file_path}")
