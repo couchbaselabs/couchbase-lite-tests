133e6602b5f59672429c914b33e27060fa92d679
G. Blake Meike <blake.meike@couchbase.com>
2025-04-25 15:37:50 -0700

    De-cruft the JAK pipelines

--- jenkins/pipelines/java/local/stop_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-ROOT_DIR="${SCRIPT_DIR}"/../../../..
-
-cd "${ROOT_DIR}"/environment
-docker compose down --rmi all 
-
