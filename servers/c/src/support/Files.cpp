765d7533c3da09ce8f2ec08d06ed685ce604f703
Jim Borden <jim.borden@couchbase.com>
2025-04-25 11:30:25 +0900

    Some changes to make the install output of C immediately usable
    Need to make sure the assets and cblite.dll are copied to the install directory.

--- servers/c/src/support/Files.cpp
+++ servers/c/src/support/Files.cpp
@@ -5,6 +5,14 @@
 #include "Android.h"
 #endif
 
+#ifdef _WIN32
+#include <windows.h>
+#elif __APPLE__
+#include <mach-o/dyld.h>
+#elif __linux__
+#include <unistd.h>
+#endif
+
 // lib
 #include <string>
 #include <filesystem>
@@ -16,6 +24,40 @@ using namespace ts::support::android;
 #endif
 
 namespace ts::support {
+    std::string getExecutablePath() {
+#ifdef _WIN32
+        char path[MAX_PATH];
+        GetModuleFileNameA(nullptr, path, MAX_PATH);
+        DWORD result = GetModuleFileNameA(nullptr, path, MAX_PATH);
+        if (result == 0) {
+            DWORD error = GetLastError();
+            throw std::runtime_error("GetModuleFileNameA failed with error code: " + std::to_string(error));
+        } else if (result == MAX_PATH) {
+            throw std::runtime_error("Path is too long and was truncated.");
+        }
+        
+        return std::string(path);
+#elif __APPLE__
+        char path[1024];
+        uint32_t size = sizeof(path);
+        if (_NSGetExecutablePath(path, &size) == 0) {
+            return std::string(path);
+        } else {
+            throw std::runtime_error("Buffer too small for executable path");
+        }
+#elif __linux__
+        char path[1024];
+        ssize_t count = readlink("/proc/self/exe", path, sizeof(path));
+        if (count != -1) {
+            return std::string(path, count);
+        } else {
+            throw std::runtime_error("Failed to get executable path");
+        }
+#else
+        throw std::runtime_error("Unsupported platform");
+#endif
+}
+
     string files::filesDir(const string &subdir, bool create) {
 #ifdef WIN32
         auto curPath = filesystem::current_path();
@@ -38,7 +80,7 @@ namespace ts::support {
 #ifdef __ANDROID__
         return androidContext()->assetsDir;
 #endif
-        auto current = filesystem::current_path() / "assets";
+        auto current = filesystem::path(getExecutablePath()).parent_path() / ".." / "assets";
         return current.string();
     }
 }
