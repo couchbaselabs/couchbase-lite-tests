d33e5a8122169a90ec0c5038ee7c248d5e1516ed
Tor Colvin <tor.colvin@couchbase.com>
2025-05-23 09:18:56 -0400

    Make mypy tests pass (#190)

--- .github/workflows/verify_python.sh
+++ .github/workflows/verify_python.sh
@@ -1,19 +1,34 @@
-#!/bin/bash -e
+#!/bin/bash
+set -eu -o pipefail
 
-python3 -m venv venv
-source ./venv/bin/activate
-pip install --upgrade pip
-pip install mypy
-pip install pytest pytest-asyncio
-pip install types-requests types-Deprecated types-tqdm types-paramiko types-netifaces types-psutil click
-pip install ./client
-echo "Checking tests files..."
-python -m mypy tests --exclude=venv
+VENV_DIR=".venv-typing"
+
+function create_venv() {
+    uv venv ${VENV_DIR}
+    source ./${VENV_DIR}/bin/activate
+}
+
+function cleanup_venv() {
+    deactivate
+    rm -rf ${VENV_DIR}
+}
+
+create_venv
+uv pip install "./client[dev]"
 echo "Checking client files (including smoke tests)..."
-python -m mypy client --exclude=venv
+${VENV_DIR}/bin/python -m mypy client
+echo "Checking tests files..."
+python -m mypy tests
+cleanup_venv
+
+create_venv
+uv pip install -r ./environment/aws/requirements.txt
 echo "Checking environment setup..."
-python -m mypy environment --exclude=venv --explicit-package-bases --check-untyped-defs
+python -m mypy environment --explicit-package-bases
+cleanup_venv
+
+create_venv
+uv pip install -r jenkins/pipelines/requirements.txt
 echo "Checking Jenkins pipelines..."
-python -m mypy jenkins --exclude=venv --explicit-package-bases
-deactivate
-rm -rf venv
+python -m mypy jenkins --explicit-package-bases
+cleanup_venv
