5d1b15c43302e93237c182bab956f2338f295906
Jim Borden <jim.borden@couchbase.com>
2025-06-13 10:52:34 +0900

    Swift prebuild support (#210)

--- jenkins/pipelines/dev_e2e/ios/test.sh
+++ jenkins/pipelines/dev_e2e/ios/test.sh
@@ -15,23 +15,20 @@ source $SCRIPT_DIR/../../shared/config.sh
 
 echo "Setup backend..."
 
+stop_venv
 create_venv venv
 source venv/bin/activate
-pip install -r $AWS_ENVIRONMENT_DIR/requirements.txt
+trap stop_venv EXIT
+uv pip install -r $AWS_ENVIRONMENT_DIR/requirements.txt
 if [ -n "$private_key_path" ]; then
     python3 $SCRIPT_DIR/setup_test.py $CBL_VERSION-$CBL_BLD_NUM $CBL_DATASET_VERSION $SGW_VERSION --private_key $private_key_path
 else
     python3 $SCRIPT_DIR/setup_test.py $CBL_VERSION-$CBL_BLD_NUM $CBL_DATASET_VERSION $SGW_VERSION
 fi
-deactivate
 
 # Run Tests :
 echo "Run tests..."
 
 pushd "${DEV_E2E_TESTS_DIR}" > /dev/null
-create_venv venv
-. venv/bin/activate
-pip install -r requirements.txt
+uv pip install -r requirements.txt
 pytest -v --no-header -W ignore::DeprecationWarning --config config.json
-deactivate
-popd > /dev/null
