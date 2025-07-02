1eb9726e9c1ce4c0f720cc4feeda180af2d99893
Sanjivani Patra <sanjivani.patra@couchbase.com>
2025-07-02 18:40:48 +0530

    Added test case for large mesh sanity

--- tests/QE/test_multipeer.py
+++ tests/QE/test_multipeer.py
@@ -17,6 +17,64 @@ logger = logging.getLogger(__name__)
 @pytest.mark.min_test_servers(2)
 class TestMultipeer(CBLTestClass):
     @pytest.mark.asyncio(loop_scope="session")
+    async def test_large_mesh_sanity(self, cblpytest: CBLPyTest):
+        self.mark_test_step("Reset local database and load `empty` dataset on all devices")
+
+        reset_tasks = [ts.create_and_reset_db(["db1"]) for ts in cblpytest.test_servers]
+        all_devices_dbs = await asyncio.gather(*reset_tasks)
+        all_dbs = [dbs[0] for dbs in all_devices_dbs]
+
+        logger.info("Created databases on all devices")
+
+        self.mark_test_step("Add 20 documents to the database on device 1")
+
+        db1 = all_dbs[0]
+
+        async with db1.batch_updater() as b:
+            for i in range(1, 21):
+                b.upsert_document(
+                    "_default._default", f"doc{i}", [{"random": randint(1, 100000)}]
+                )
+
+        logger.info("Added 20 documents to the database on device 1")
+
+        self.mark_test_step("Start multipeer replicator on all devices")
+        multipeer_replicators = [
+            MultipeerReplicator(
+                "mesh-test", db, [ReplicatorCollectionEntry(["_default._default"])]
+            )
+            for db in all_dbs
+        ]
+        mpstart_tasks = [multipeer.start() for multipeer in multipeer_replicators]
+        await asyncio.gather(*mpstart_tasks)
+
+        logger.info("Started multipeer replicator on all devices")
+
+        self.mark_test_step("Wait for idle status on all devices")
+        for multipeer in multipeer_replicators[1:]:
+            status = await multipeer.wait_for_idle()
+            assert all(r.status.replicator_error is None for r in status.replicators), (
+                "Multipeer replicator should not have any errors"
+            )
+
+        self.mark_test_step("Check that all device databases have the same content")
+
+        all_docs_collection = [
+            db.get_all_documents("_default._default") for db in all_dbs
+        ]
+        all_docs_results = await asyncio.gather(*all_docs_collection)
+        for all_docs in all_docs_results[1:]:
+            assert compare_doc_results_p2p(
+                all_docs_results[0]["_default._default"], all_docs["_default._default"]
+            ), "All databases should have the same content"
+
+        await asyncio.gather(*[multipeer.stop() for multipeer in multipeer_replicators])
+
+        logger.info("All multipeer replicators stopped")
+        logger.info("Successfully completed test_large_mesh_sanity")
+    
+
+    @pytest.mark.asyncio(loop_scope="session")
     async def test_large_mesh_consistency(self, cblpytest: CBLPyTest):
         self.mark_test_step("Reset local database and load `empty` dataset on all devices")
 
