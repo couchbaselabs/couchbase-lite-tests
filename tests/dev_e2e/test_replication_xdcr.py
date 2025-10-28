98938701d62fdd7fca7f14a217aed5f0d626705b
Pasin <pasin@couchbase.com>
2025-10-27 20:13:25 -0700

    Fix xdcr and upgrade tests to skip when test server is not 4.0.0

--- tests/dev_e2e/test_replication_xdcr.py
+++ tests/dev_e2e/test_replication_xdcr.py
@@ -68,6 +68,8 @@ class TestReplicationXdcr(CBLTestClass):
     async def test_push_and_pull_with_xdcr(
         self, cblpytest: CBLPyTest, dataset_path: Path
     ):
+        self.skip_if_cbl_not(cblpytest.test_servers[0], ">= 4.0.0")
+
         if len(cblpytest.load_balancers) < 1:
             pytest.skip("No load balancer available for XDCR test.")
 
@@ -215,6 +217,8 @@ class TestReplicationXdcr(CBLTestClass):
 
     @pytest.mark.asyncio(loop_scope="session")
     async def test_fail_over(self, cblpytest: CBLPyTest, dataset_path: Path):
+        self.skip_if_cbl_not(cblpytest.test_servers[0], ">= 4.0.0")
+
         if len(cblpytest.load_balancers) < 1:
             pytest.skip("No load balancer available for XDCR test.")
 
