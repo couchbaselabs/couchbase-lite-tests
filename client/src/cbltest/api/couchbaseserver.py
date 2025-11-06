00ca1c4e508adeca6dd5a0a53ebc8f6a9e711451
Jim Borden <jim.borden@couchbase.com>
2025-11-07 08:38:00 +0900

    One small fix with replicator and undeprecate SGW functions (#300)
    When calling add_default_collection it should add _default._default, not just _default
    
    Since 4.0 GA Sync Gateway didn't come with CV bulk write support, need to keep the workaround in place longer but undeprecate it so it doesn't spew warnings
    
    * Added a custom skip function
    
    This way there are no spurious "no test steps" warnings when a test is skipped directly via pytest skip
    
    * Move off of deprecate server SDK method

--- client/src/cbltest/api/couchbaseserver.py
+++ client/src/cbltest/api/couchbaseserver.py
@@ -20,7 +20,6 @@ from couchbase.exceptions import (
     ScopeAlreadyExistsException,
 )
 from couchbase.management.buckets import CreateBucketSettings
-from couchbase.management.collections import CollectionSpec
 from couchbase.management.options import CreatePrimaryQueryIndexOptions
 from couchbase.options import ClusterOptions
 from couchbase.subdocument import upsert
@@ -86,7 +85,7 @@ class CouchbaseServer:
                 ):
                     try:
                         if name != "_default":
-                            c.create_collection(CollectionSpec(name, scope))
+                            c.create_collection(scope_name=scope, collection_name=name)
                     except CollectionAlreadyExistsException:
                         pass
 
