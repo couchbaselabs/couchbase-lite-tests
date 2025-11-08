9a34cfa99736d54c1ce7078a5f3bbdc1d1600488
Jim Borden <jim.borden@couchbase.com>
2025-11-09 07:55:43 +0900

    Merge Push and Pull filter definitions
    Now that the function signature is same again it is redundant to define them separately.

--- servers/javascript/src/filters.ts
+++ servers/javascript/src/filters.ts
@@ -5,57 +5,32 @@ import {check, isObject} from "./utils";
 
 // https://github.com/couchbaselabs/couchbase-lite-tests/blob/main/spec/api/replication-filters.md
 
-type PushReplicatorFilter = (doc: cbl.CBLDocument, flags: cbl.DocumentFlags) => boolean;
-type PushFilterMaker = (spec: tdk.Filter) => PushReplicatorFilter;
-type PullReplicatorFilter = (doc: cbl.CBLDocument, flags: cbl.DocumentFlags) => boolean;
-type PullFilterMaker = (spec: tdk.Filter) => PullReplicatorFilter;
+type ReplicatorFilter = (doc: cbl.CBLDocument, flags: cbl.DocumentFlags) => boolean;
+type FilterMaker = (spec: tdk.Filter) => ReplicatorFilter;
 
-function _documentIDs(spec: tdk.Filter, doc: cbl.CBLDocument, _flags: cbl.DocumentFlags) : boolean {
-    const documentIDs = spec.params?.documentIDs;
-    check(isObject(documentIDs), "documentIDs must be an object");
-
-    const meta = cbl.meta(doc);
-    const ids = documentIDs[meta.collection.name];
-    check(Array.isArray(ids), "documentIDs's value must be an array");
-
-    const docSet = new Set(ids);
-    return docSet.has(meta.id);
-}
-
-function _deletedDocumentsOnly(_doc: cbl.CBLDocument, flags: cbl.DocumentFlags) : boolean {
-    return (flags & cbl.DocumentFlags.deleted) !== 0;
-}
-
-export const TDKPushReplicationFilters: Record<string,PushFilterMaker> = {
+export const TDKReplicationFilters: Record<string,FilterMaker> = {
     documentIDs: (spec) => {
-        return (doc, flags) => _documentIDs(spec, doc, flags);
+        return (doc, _) => {
+            const documentIDs = spec.params?.documentIDs;
+            check(isObject(documentIDs), "documentIDs must be an object");
+
+            const meta = cbl.meta(doc);
+            const ids = documentIDs[meta.collection.name];
+            check(Array.isArray(ids), "documentIDs's value must be an array");
+
+            const docSet = new Set(ids);
+            return docSet.has(meta.id);
+        };
     },
 
     deletedDocumentsOnly: (_spec) => {
-        return (doc, flags) => _deletedDocumentsOnly(doc, flags);
+        return (_, flags) => (flags & cbl.DocumentFlags.deleted) !== 0;
     }
 };
 
-export const TDKPullReplicationFilters: Record<string,PullFilterMaker> = {
-    documentIDs: (spec) => {
-        return (doc, flags) => _documentIDs(spec, doc, flags);
-    },
-
-    deletedDocumentsOnly: (_spec) => {
-        return (doc, flags) => _deletedDocumentsOnly(doc, flags);
-    }
-};
-
-export function CreatePushFilter(filterSpec: tdk.Filter | undefined): PushReplicatorFilter | undefined {
+export function CreateFilter(filterSpec: tdk.Filter | undefined): ReplicatorFilter | undefined {
     if (!filterSpec) return undefined;
-    const filterFn = TDKPushReplicationFilters[filterSpec.name]?.(filterSpec);
-    check(filterFn !== undefined, `Unknown replicator filter "${filterSpec.name}"`);
-    return filterFn;
-}
-
-export function CreatePullFilter(filterSpec: tdk.Filter | undefined): PullReplicatorFilter | undefined {
-    if (!filterSpec) return undefined;
-    const filterFn = TDKPullReplicationFilters[filterSpec.name]?.(filterSpec);
+    const filterFn = TDKReplicationFilters[filterSpec.name]?.(filterSpec);
     check(filterFn !== undefined, `Unknown replicator filter "${filterSpec.name}"`);
     return filterFn;
 }
