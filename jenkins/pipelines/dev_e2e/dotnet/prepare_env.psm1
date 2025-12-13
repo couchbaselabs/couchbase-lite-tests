49fde6c775d270ea9263ea24957db3f6df70b58f
Jim Borden <jim.borden@couchbase.com>
2025-12-13 11:23:00 +0900

    Fix script errors on Windows
    Also split .NET SDK and runtime install

--- jenkins/pipelines/dev_e2e/dotnet/prepare_env.psm1
+++ jenkins/pipelines/dev_e2e/dotnet/prepare_env.psm1
@@ -1,2 +1,12 @@
-Invoke-WebRequest https://raw.githubusercontent.com/couchbaselabs/couchbase-mobile-tools/refs/heads/master/dotnet_testing_env/prepare_dotnet.psm1 -OutFile $PSScriptRoot/prepare_dotnet.psm1
-Import-Module $PSScriptRoot/prepare_dotnet.psm1 -Force
+
+$target = Join-Path $PSScriptRoot 'prepare_dotnet.psm1'
+
+if (-not (Test-Path -Path $target)) {
+	Invoke-WebRequest \
+		-Uri https://raw.githubusercontent.com/couchbaselabs/couchbase-mobile-tools/refs/heads/master/dotnet_testing_env/prepare_dotnet.psm1 \
+		-OutFile $target
+} else {
+	Write-Host "Using existing prepare_dotnet.psm1 at $target"
+}
+
+$mod = Import-Module $target -Force
