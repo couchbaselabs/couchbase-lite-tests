a12c03ecc0ee7c544645152b8f9d64e9d5947882
Jim Borden <jim.borden@couchbase.com>
2025-12-13 11:30:50 +0900

    Incorrect multiline separators

--- jenkins/pipelines/dev_e2e/dotnet/prepare_env.psm1
+++ jenkins/pipelines/dev_e2e/dotnet/prepare_env.psm1
@@ -2,8 +2,8 @@
 $target = Join-Path $PSScriptRoot 'prepare_dotnet.psm1'
 
 if (-not (Test-Path -Path $target)) {
-	Invoke-WebRequest \
-		-Uri https://raw.githubusercontent.com/couchbaselabs/couchbase-mobile-tools/refs/heads/master/dotnet_testing_env/prepare_dotnet.psm1 \
+	Invoke-WebRequest `
+		-Uri https://raw.githubusercontent.com/couchbaselabs/couchbase-mobile-tools/refs/heads/master/dotnet_testing_env/prepare_dotnet.psm1 `
 		-OutFile $target
 } else {
 	Write-Host "Using existing prepare_dotnet.psm1 at $target"
