56aa56f2d96a690415f0c074a4024bacf7bbfc86
Sanjivani Patra <sanjivani.patra@couchbase.com>
2025-12-14 22:45:32 +0530

    Added fix for the greenboard issue seen on jenkins

--- client/src/cbltest/plugins/greenboard_fixture.py
+++ client/src/cbltest/plugins/greenboard_fixture.py
@@ -2,7 +2,7 @@ import pytest
 import pytest_asyncio
 from cbltest import CBLPyTest
 from cbltest.greenboarduploader import GreenboardUploader
-from cbltest.logging import cbl_info
+from cbltest.logging import cbl_info, cbl_warning
 
 # This plugin provides an automatic (i.e. not used directly by tests)
 # fixture that will upload test results to greenboard, if it is
@@ -44,20 +44,22 @@ async def greenboard(cblpytest: CBLPyTest, pytestconfig: pytest.Config):
     # is 'session' it basically means "before and after the run"
     yield
 
-    test_server_info = await cblpytest.test_servers[0].get_info()
-    sgw_version: str = "n/a"
-    if len(cblpytest.sync_gateways) > 0:
-        sgw_version_parts = await cblpytest.sync_gateways[0].get_version()
-        sgw_version = f"{sgw_version_parts.version}-{sgw_version_parts.build_number}"
-    os_name = (
-        test_server_info.device["systemName"]
-        if "systemName" in test_server_info.device
-        else ""
-    )
-    uploader.upload(
-        test_server_info.cbl, os_name, test_server_info.library_version, sgw_version
-    )
-    pytestconfig.pluginmanager.unregister(uploader)
+    try:
+        test_server_info = await cblpytest.test_servers[0].get_info()
+        sgw_version: str = "n/a"
+        if len(cblpytest.sync_gateways) > 0:
+            sgw_version_parts = await cblpytest.sync_gateways[0].get_version()
+            sgw_version = f"{sgw_version_parts.version}-{sgw_version_parts.build_number}"
+        os_name = (
+            test_server_info.device["systemName"] if "systemName" in test_server_info.device else ""
+        )
+        uploader.upload(
+            test_server_info.cbl, os_name, test_server_info.library_version, sgw_version
+        )
+    except Exception as e:
+        cbl_warning(f"Failed to upload results to Greenboard: {e}")
+    finally:
+        pytestconfig.pluginmanager.unregister(uploader)
 
 
 # This adds the --no-result-upload option to the pytest command line.
