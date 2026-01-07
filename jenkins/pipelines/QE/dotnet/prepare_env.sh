f70ad034e0db21db1abf76b4b344058ef4f2eff8
Sanjivani Patra <sanjivani.patra@couchbase.com>
2026-01-07 21:06:23 +0530

    Changed .net version to 8.0 in prepare_env

--- jenkins/pipelines/QE/dotnet/prepare_env.sh
+++ jenkins/pipelines/QE/dotnet/prepare_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 export DOTNET_ROOT=$HOME/.dotnet
-export DOTNET_VERSION="9.0"
+export DOTNET_VERSION="8.0"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 PREPARE_DOTNET_SCRIPT="$SCRIPT_DIR/prepare_dotnet.sh"
