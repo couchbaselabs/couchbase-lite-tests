d78f29f6b71d8aba70b88f677382e13902b55b93
barkha06 <barkha.goyal@couchbase.com>
2026-01-16 11:58:15 +0530

    Added assertions to test_basic_auth

--- tests/QE/edge_server/test_authentication.py
+++ tests/QE/edge_server/test_authentication.py
@@ -17,7 +17,6 @@ class TestAuthentication(CBLTestClass):
         valid_auth = ("username8", "password8")
         invalid_auth = ("invalid_user", "wrong_password")
 
-        #     testing valid auth
         self.mark_test_step("testing valid auth")
         await edge_server.add_user(name=valid_auth[0], password=valid_auth[1])
         await edge_server.set_auth(name=valid_auth[0], password=valid_auth[1])
@@ -25,16 +24,21 @@ class TestAuthentication(CBLTestClass):
         active_tasks = await edge_server.get_active_tasks()
         self.mark_test_step(f"Active Tasks: {active_tasks}")
 
-        # testing invalid auth
         self.mark_test_step("testing invalid auth")
         await edge_server.set_auth(name=invalid_auth[0], password=invalid_auth[1])
+        failed = False
         try:
             await edge_server.get_active_tasks()
         except Exception:
+            failed = True
             self.mark_test_step("invalid auth failed as expected")
+        assert failed, "invalid auth did not fail as expected"
         self.mark_test_step("testing anonymous auth ")
         await edge_server.set_auth(auth=False)
+        failed = False
         try:
             await edge_server.get_active_tasks()
         except Exception:
+            failed = True
             self.mark_test_step("No auth failed as expected")
+        assert failed, "No auth did not fail as expected"
