f36ef34d27677db344e7d17192df30dd97dace02
Sanjivani Patra <sanjivani.patra@couchbase.com>
2026-01-19 19:52:29 +0530

    Updated the powershell run_test script

--- jenkins/pipelines/QE/c/run_test.ps1
+++ jenkins/pipelines/QE/c/run_test.ps1
@@ -7,20 +7,25 @@ $ErrorActionPreference = "Stop"
 
 Import-Module $PSScriptRoot\..\..\shared\config.psm1 -Force
 
-python -m venv venv
+Stop-Venv
+New-Venv venv
 .\venv\Scripts\activate
-pip install -r $AWS_ENVIRONMENT_DIR\requirements.txt
+trap { Stop-Venv; break }
+uv pip install -r $AWS_ENVIRONMENT_DIR\requirements.txt
 python $PSScriptRoot\setup_test.py "windows" $Version $SgwVersion
 if($LASTEXITCODE -ne 0) {
     throw "Setup failed!"
 }
 
 Push-Location $QE_TESTS_DIR
-pip install -r requirements.txt
-pytest -v --no-header -W ignore::DeprecationWarning --config config.json -m cbl
-$saved_exit = $LASTEXITCODE
-deactivate
-Pop-Location
+try {
+    uv pip install -r requirements.txt
+    pytest -v --no-header -W ignore::DeprecationWarning --config config.json -m cbl
+    $saved_exit = $LASTEXITCODE
+    deactivate
+} finally {
+    Pop-Location
+}
 
 if($saved_exit -ne 0) {
     throw "Testing failed!"
