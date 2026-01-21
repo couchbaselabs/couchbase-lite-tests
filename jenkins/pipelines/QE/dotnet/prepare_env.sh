6ff06f2069e0374a28c10f11af6a27a0435fbd3b
Sanjivani Patra <sanjivani.patra@couchbase.com>
2026-01-21 18:23:41 +0530

    Changed it to always download fresh script

--- jenkins/pipelines/QE/dotnet/prepare_env.sh
+++ jenkins/pipelines/QE/dotnet/prepare_env.sh
@@ -7,9 +7,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 PREPARE_DOTNET_SCRIPT="$SCRIPT_DIR/prepare_dotnet.sh"
 
-if [ ! -f $PREPARE_DOTNET_SCRIPT ]; then
-    echo "Downloading prepare_dotnet.sh..."
-    curl -L https://raw.githubusercontent.com/couchbaselabs/couchbase-mobile-tools/refs/heads/master/dotnet_testing_env/prepare_dotnet_new.sh -o $PREPARE_DOTNET_SCRIPT
-fi
+echo "Downloading prepare_dotnet.sh..."
+curl -L https://raw.githubusercontent.com/couchbaselabs/couchbase-mobile-tools/refs/heads/master/dotnet_testing_env/prepare_dotnet_new.sh -o $PREPARE_DOTNET_SCRIPT
 
 source $PREPARE_DOTNET_SCRIPT
