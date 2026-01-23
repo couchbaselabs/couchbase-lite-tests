f53c3a4fb5a9d8806c64e5963f417518f0d81653
barkha06 <barkha.goyal@couchbase.com>
2026-01-23 11:09:07 +0530

    Added sudo to kill-edgeserver.sh

--- environment/aws/es_setup/shell2http/kill-edgeserver.sh
+++ environment/aws/es_setup/shell2http/kill-edgeserver.sh
@@ -10,5 +10,5 @@ sudo kill -SIGHUP $PID
 if curl -fs "http://127.0.0.1:59840" >/dev/null 2>&1; then
   echo "Edge server is still running"
   PIDS=$(pgrep -f "/opt/couchbase-edge-server/bin/couchbase-edge-server" || true)
-  kill -TERM $PIDS || true
+  sudo kill -TERM $PIDS || true
 fi
