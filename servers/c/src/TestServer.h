c489757bcd84810fbe190096855f8e0c4b602b2d
barkha06 <barkha.goyal@couchbase.com>
2026-02-16 20:55:22 +0530

    setting port back to 8080

--- servers/c/src/TestServer.h
+++ servers/c/src/TestServer.h
@@ -15,7 +15,7 @@ namespace ts {
     public:
         static constexpr const char *CBL_PLATFORM_NAME = "couchbase-lite-c";
         static constexpr unsigned short API_VERSION = 1;
-        static constexpr unsigned short PORT = 8081;
+        static constexpr unsigned short PORT = 8080;
 
         struct Context {
             std::string filesDir;
