fbf097ca8cca70efdc41dc05a6cad1cf0391684d
Jim Borden <jim.borden@couchbase.com>
2026-04-02 12:34:58 +0900

    Temporarily disable git lfs checkout check

--- tests/dev_e2e/conftest.py
+++ tests/dev_e2e/conftest.py
@@ -9,6 +9,6 @@ from cbltest.utils import verify_lfs_checkout
 # into tests that need it.
 @pytest.fixture(scope="session")
 def dataset_path() -> Path:
-    verify_lfs_checkout()
+    #verify_lfs_checkout()
     script_path = os.path.abspath(os.path.dirname(__file__))
     return Path(script_path, "..", "..", "dataset", "sg")
