314cd7ba97bb9b5f8cfe4fdac2ed66f542fd924f
Jim Borden <jim.borden@couchbase.com>
2026-05-09 10:15:22 +0900

    Add forgotten files

---
+++ jenkins/pipelines/prebuild/setup.psm1
@@ -0,0 +1,9 @@
+if (-not $env:TS_PLATFORM) {
+    Write-Error "Error: TS_PLATFORM environment variable is not set."
+    exit 1
+}
+
+Import-Module $PSScriptRoot/../shared/config.psm1 -Force
+if ($env:TS_PLATFORM -like "dotnet*") {
+    Import-Module "$PSScriptRoot\setup_dotnet.ps1" -Force
+}
